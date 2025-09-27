3a45d376c2debc487c08d4ad88bac42d72cda0a8
MesCash Admin <admin@mescash.ci>
2025-09-27 05:11:32 +0000

    style(presentation): Make events section background transparent

--- resources/views/homepage/_events.blade.php
+++ resources/views/homepage/_events.blade.php
@@ -1,5 +1,5 @@
 <!-- Events & Partners Section -->
-<div class="bg-gray-900 py-24 sm:py-32">
+<div class="py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl lg:text-center">
             <h2 class="text-base font-semibold leading-7 text-orange-500">Partenariats & Événements</h2>
