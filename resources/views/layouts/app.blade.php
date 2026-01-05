cf811ef7373b74623b9b2b13d0da5979caf11659
MesCash Admin <admin@mescash.ci>
2026-01-05 23:17:22 +0000

    Chore: utiliser favicon depuis assets/img/favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,9 +48,9 @@
     <meta name="apple-mobile-web-app-title" content="EVC">
 
     <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicon/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicon/favicon.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicon/favicon.png') }}">
     <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
