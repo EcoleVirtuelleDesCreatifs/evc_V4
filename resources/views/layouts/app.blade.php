940b8ba1ca4cffed47be392f20c0e0a62c6f65b4
MesCash Admin <admin@mescash.ci>
2026-06-04 02:49:12 +0000

    Topbar: bouton Faire un don pointe vers /faire-un-don

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -345,7 +345,7 @@
                 <div class="evc-topbar-sep"></div>
 
                 {{-- Droite : Faire un don (CTA impactant) --}}
-                <a href="mailto:info@ecolevirtuelledescreatifs.com?subject=Don%20EVC" class="evc-topbar-don">
+                <a href="{{ url('/faire-un-don') }}" class="evc-topbar-don">
                     <svg width="12" height="12" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"/></svg>
                     ✨ FAIRE UN DON
                 </a>
