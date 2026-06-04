7c643b0ad6b1ea1bd2f0b24e37ef050a3d48c01d
MesCash Admin <admin@mescash.ci>
2026-06-04 02:47:21 +0000

    Topbar: email mis à jour → info@ecolevirtuelledescreatifs.com

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -310,9 +310,9 @@
             <div class="h-full flex items-center justify-between gap-2">
 
                 {{-- Gauche : Email contact --}}
-                <a href="mailto:contact@ecolevirtuelledescreatifs.com" class="evc-topbar-email evc-topbar-hide-xs">
+                <a href="mailto:info@ecolevirtuelledescreatifs.com" class="evc-topbar-email evc-topbar-hide-xs">
                     <svg width="12" height="12" viewBox="0 0 20 20" fill="currentColor"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/></svg>
-                    contact@ecolevirtuelledescreatifs.com
+                    info@ecolevirtuelledescreatifs.com
                 </a>
 
                 {{-- Séparateur --}}
@@ -345,7 +345,7 @@
                 <div class="evc-topbar-sep"></div>
 
                 {{-- Droite : Faire un don (CTA impactant) --}}
-                <a href="mailto:contact@ecolevirtuelledescreatifs.com?subject=Don%20EVC" class="evc-topbar-don">
+                <a href="mailto:info@ecolevirtuelledescreatifs.com?subject=Don%20EVC" class="evc-topbar-don">
                     <svg width="12" height="12" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"/></svg>
                     ✨ FAIRE UN DON
                 </a>
