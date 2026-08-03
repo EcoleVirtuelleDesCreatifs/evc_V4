8f4a470f8009c499371176d6e9d6ddca45f58932
MesCash Admin <admin@mescash.ci>
2026-08-03 07:54:36 +0000

    Header: réduction de la hauteur du menu de 90px à 70px

--- resources/views/homepage/_header.blade.php
+++ resources/views/homepage/_header.blade.php
@@ -411,7 +411,7 @@
 
 <!-- Header -->
 <header id="main-header" class="bg-white fixed top-0 left-0 w-full z-50 shadow-sm">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between px-3 py-1 lg:px-8 lg:py-0" style="min-height:90px;">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between px-3 py-1 lg:px-8 lg:py-0" style="min-height:70px;">
         <div class="flex lg:flex-1">
             <a href="{{ url('/') }}">
                 <img class="h-16 logo-desktop w-auto transition-all duration-300" src="{{ asset('assets/img/logo_evc.png') }}"
