5c8b2fb3895c656e9168a25b2b3c5672dfdd97be
MesCash Admin <admin@mescash.ci>
2026-08-05 18:56:34 +0000

    Admissions: augmentation du margin-top pour faire descendre encore le titre 'Admissions'

--- resources/views/admissions/index.blade.php
+++ resources/views/admissions/index.blade.php
@@ -29,7 +29,7 @@
 
     .admissions-hero {
         text-align: center;
-        margin-top: 60px;
+        margin-top: 120px;
         margin-bottom: 40px;
         position: relative;
         z-index: 1;
